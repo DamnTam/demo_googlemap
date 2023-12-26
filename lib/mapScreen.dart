493d0a829b76dc9585bf8d742b182a7679c8740f
DamnTam <tanvirmoon33@gmail.com>
2023-12-27 00:57:36 +0600

    :)

--- lib/mapScreen.dart
+++ lib/mapScreen.dart
@@ -17,11 +17,8 @@ class _MapScreenState extends State<MapScreen> {
   Location location = Location();
   List<LatLng> polylineCoordinates = [];
   late StreamSubscription locationSubscription;
-  LatLng ? previousLocation;
-  final CameraPosition _initialCameraPosition = const CameraPosition(
-    target: LatLng(23.6850, 90.3563),
-    zoom: 13.0,
-  );
+  LatLng? previousLocation;
+  PermissionStatus? permissionStatus;
 
   @override
   void initState() {
@@ -30,20 +27,28 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> requestPermission() async {
-    await location.requestPermission().then((granted) {
+    await location.requestPermission().then((granted) async {
       if (granted == PermissionStatus.granted) {
-        locationSubscription = location.onLocationChanged.listen((LocationData onChangeLocation) {
+        locationSubscription =
+            location.onLocationChanged.listen((LocationData onChangeLocation) {
           log(onChangeLocation.latitude.toString());
           log(onChangeLocation.longitude.toString());
-
           currentLocation = onChangeLocation;
           updatePolyline();
           animateCamera(zoom: 19.5);
+
           ///fetch the user current location every 10 seconds
           locationSubscription.pause(Future.delayed(const Duration(seconds: 10))
               .then((value) => locationSubscription.resume()));
           setState(() {});
         });
+      } else {
+        permissionStatus = PermissionStatus.denied;
+        setState(() {});
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('Permission not granted'),
+        ));
       }
     });
   }
@@ -63,7 +68,7 @@ class _MapScreenState extends State<MapScreen> {
 
   void updatePolyline() {
     if (polylineCoordinates.isNotEmpty) {
-       previousLocation = polylineCoordinates.last;
+      previousLocation = polylineCoordinates.last;
       log('previousLocation: $previousLocation');
       LatLng currentLatLng = LatLng(
           double.parse(currentLocation?.latitude.toString() ?? '0.0'),
@@ -73,8 +78,7 @@ class _MapScreenState extends State<MapScreen> {
           polylineCoordinates.add(currentLatLng);
         });
       }
-    }
-    else {
+    } else {
       // First time adding current location to polylineCoordinates
       polylineCoordinates.add(LatLng(
           double.parse(currentLocation?.latitude.toString() ?? '0.0'),
@@ -93,64 +97,79 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 5,
-          title: const Text('Flutter Map App'),
-        ),
-        body: currentLocation == null
-            ? const Center(
-                child: CircularProgressIndicator(color: Colors.black,),
-              )
-            : Stack(
-                children: [
-                  GoogleMap(
-                    onMapCreated: (GoogleMapController controller) {
-                      setState(() {
-                        mapController = controller;
-                      });
-                    },
-                    zoomControlsEnabled: false,
-                    initialCameraPosition: _initialCameraPosition,
-                    myLocationEnabled: false,
-                    myLocationButtonEnabled: false,
-                    markers: {
-                      Marker(
-                        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-                        markerId: const MarkerId('MyLocation'),
-                        position: LatLng(currentLocation!.latitude!.toDouble(),
-                            currentLocation!.longitude!.toDouble()),
-                        onTap: () {
-                          // Show info window when marker is tapped
-                          mapController.showMarkerInfoWindow(const MarkerId('MyLocation'));
-                        },
-                        infoWindow: InfoWindow(
-                          title: 'My current location',
-                          snippet:
-                              'Lat: ${currentLocation!.latitude}, Lng: ${currentLocation!.longitude}',
-                        ),
-                      ),
-
-                    },
-                    polylines: {
-                      Polyline(
-                        color: Colors.black,
-                        polylineId: const PolylineId('Polyline'),
-                        points: polylineCoordinates,
-
-                      ),
-                    },
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 5,
+        title: const Text('Flutter Map App'),
+      ),
+      body: permissionStatus == PermissionStatus.denied
+          ? const Center(
+              child: Text('Permission not granted!!!Checkout app settings',
+                  style: TextStyle(color: Colors.red, fontSize: 20)))
+          : currentLocation == null
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
                   ),
-                ],
-              ),
+                )
+              : Stack(
+                  children: [
+                    GoogleMap(
+                      onMapCreated: (GoogleMapController controller) {
+                        setState(() {
+                          mapController = controller;
+                        });
+                      },
+                      zoomControlsEnabled: false,
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(
+                            double.parse(
+                                currentLocation?.latitude.toString() ?? '0.0'),
+                            double.parse(
+                                currentLocation?.longitude.toString() ??
+                                    '0.0')),
+                        zoom: 15,
+                      ),
+                      myLocationEnabled: false,
+                      myLocationButtonEnabled: false,
+                      markers: {
+                        Marker(
+                          icon: BitmapDescriptor.defaultMarkerWithHue(
+                              BitmapDescriptor.hueGreen),
+                          markerId: const MarkerId('MyLocation'),
+                          position: LatLng(
+                              currentLocation!.latitude!.toDouble(),
+                              currentLocation!.longitude!.toDouble()),
+                          onTap: () {
+                            // Show info window when marker is tapped
+                            mapController.showMarkerInfoWindow(
+                                const MarkerId('MyLocation'));
+                          },
+                          infoWindow: InfoWindow(
+                            title: 'My current location',
+                            snippet:
+                                'Lat: ${currentLocation!.latitude}, Lng: ${currentLocation!.longitude}',
+                          ),
+                        ),
+                      },
+                      polylines: {
+                        Polyline(
+                          color: Colors.black,
+                          polylineId: const PolylineId('Polyline'),
+                          points: polylineCoordinates,
+                        ),
+                      },
+                    ),
+                  ],
+                ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (currentLocation != null) {
-            animateCamera(zoom: 17);
+            animateCamera(zoom: 17.5);
           }
         },
         child: const Icon(Icons.center_focus_strong),
       ),
-        );
+    );
   }
 }
